78a8ccd0d37807c6a59796059c1c9a588d7cc603
MottinDev <qwertyukj@gmail.com>
2024-09-09 00:25:13 -0400

    chore: Atualizar URL de conexão do MongoDB.

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/todoDB');
+    const conn = await mongoose.connect('mongodb+srv://Mottin:Noobert123@cluster0.xw0e7.mongodb.net/userMongo?retryWrites=true&w=majority&appName=Cluster0');
     console.log(`MongoDB conectado: ${conn.connection.host}`);
   } catch (error : any) {
     console.error(`Erro ao conectar ao MongoDB: ${error.message}`);
